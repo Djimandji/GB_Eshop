34c4a591e1c000569de289e9be489f1905efcda1
Zagryadskiy Dmitriy <zagryadskiy.dima@gmail.com>
2022-04-23 17:06:57 +0500

    lesson 8 base

---
+++ shop-picture-service-api/src/test/java/ru/geekbrains/shoppictureserviceapi/ShopPictureServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.shoppictureserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class ShopPictureServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
